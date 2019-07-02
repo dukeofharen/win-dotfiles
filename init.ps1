b4d7da22eb8f852f805f0d9fa45ecaff04a3df2b
Duco <mail@ducode.org>
2019-07-02 13:08:06 +0200

    Added CreateNewTextFile installation to init.ps1

--- init.ps1
+++ init.ps1
@@ -1,6 +1,9 @@
 # Install VSCode settings
 . "$PSScriptRoot\VSCode\VSCode-Init.ps1"
 
+# Install CreateNewTextFile
+. "$PSScriptRoot\Macro\CreateNewTextFile\Install.ps1"
+
 # Show profile installation message to user
 Write-Host -ForegroundColor Green "To have access to all the PowerShell functions, add the following line to your PowerShell profile script ($profile)"
 Write-Host -ForegroundColor Green ". ""$PSScriptRoot\ProfileScripts\Load-Profile.ps1"""
