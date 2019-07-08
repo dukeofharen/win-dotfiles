256ff13a3955cc3cc7a1ebc1db039f12ba08be7e
Duco <mail@ducode.org>
2019-07-08 09:26:04 +0200

    Added functions for stopping and starting DBs

--- ProfileScripts/Exe-Functions.ps1
+++ ProfileScripts/Exe-Functions.ps1
@@ -1,6 +1,27 @@
 function d-restart-sql {
-    & net stop 'MSSQL$SQLEXPRESS'
-    & net start 'MSSQL$SQLEXPRESS'
+    & net stop 'MSSQLSERVER'
+    & net start 'MSSQLSERVER'
+}
+
+function d-stop-sql {
+    & net stop 'MSSQLSERVER'
+}
+
+function d-start-sql {
+    & net start 'MSSQLSERVER'
+}
+
+function d-restart-mysql {
+    & net stop 'MySQL80'
+    & net start 'MySQL80'
+}
+
+function d-stop-mysql {
+    & net stop 'MySQL80'
+}
+
+function d-start-mysql {
+    & net start 'MySQL80'
 }
 
 function d-kill([Parameter(Position = 0)][string]$app) {
